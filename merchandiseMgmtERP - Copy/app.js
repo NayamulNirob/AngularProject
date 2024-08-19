d1c9f84c5528abf896db95f0e5297910c1422297
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-08-20 01:22:02 +0600

    From home 1:16AM
    20/08/2024

---
+++ merchandiseMgmtERP - Copy/app.js
@@ -0,0 +1,12 @@
+// const express=require('express');
+// const { server } = require('typescript');
+// const app =express(); 
+
+
+// const prot=5005;
+
+// app.use(express.static('public'));
+
+// app.listen(prot,()=>{
+//     console.log(`server successfull, listeing on port${port}`)
+// })
---
+++ merchandiseMgmtERP - Copy/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
